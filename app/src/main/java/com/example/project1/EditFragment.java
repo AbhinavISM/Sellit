e77c2bc11c6e0980e6602704a028fbe2c3bc4f6d
AbhinavISM <111358311+AbhinavISM@users.noreply.github.com>
2022-08-29 23:27:23 +0530

    some data binding implemented without view model or live data in edit fragment.

--- app/src/main/java/com/example/project1/EditFragment.java
+++ app/src/main/java/com/example/project1/EditFragment.java
@@ -9,19 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.project1.databinding.FragmentEditBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +30,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,52 +38,57 @@ import java.util.Map;
 
 public class EditFragment extends Fragment {
 
-    EditText edit_phoneno;
-    EditText edit_adress;
-    EditText edit_price;
-    EditText edit_details;
+//    EditText edit_phoneno;
+//    EditText edit_adress;
+//    EditText edit_price;
+//    EditText edit_details;
     String offered_by;
-    ImageView edit_Image;
+//    ImageView edit_Image;
     Uri edit_image_uri;
     String image_url;
     String property_ID;
     String property_ID_particular;
     int adapter_position;
-
-    Button update_button;
-    Button delete_button;
+    FragmentEditBinding fragmentEditBinding;
+//    Button update_button;
+//    Button delete_button;
 
     PropertyFragmentViewModel propertyFragmentViewModel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false);
+        fragmentEditBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_edit,container,false);
+        View view = fragmentEditBinding.getRoot();
+        fragmentEditBinding.setLifecycleOwner(this);
+        propertyFragmentViewModel = new ViewModelProvider(getActivity()).get(PropertyFragmentViewModel.class);
+        fragmentEditBinding.setViewmodel(propertyFragmentViewModel);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        edit_phoneno = getView().findViewById(R.id.editphoneno_fragment);
-        edit_Image = getView().findViewById(R.id.editimage_fragment);
-        edit_adress = getView().findViewById(R.id.editadress_fragment);
-        edit_price = getView().findViewById(R.id.editprice_fragment);
-        edit_details = getView().findViewById(R.id.editdetails_fragment);
-        update_button = getView().findViewById(R.id.updatebutton_fragment);
-        delete_button = getView().findViewById(R.id.deletebutton_fragment);
+//        edit_phoneno = getView().findViewById(R.id.editphoneno_fragment);
+//        edit_Image = getView().findViewById(R.id.editimage_fragment);
+//        edit_adress = getView().findViewById(R.id.editadress_fragment);
+//        edit_price = getView().findViewById(R.id.editprice_fragment);
+//        edit_details = getView().findViewById(R.id.editdetails_fragment);
+//        update_button = getView().findViewById(R.id.updatebutton_fragment);
+//        delete_button = getView().findViewById(R.id.deletebutton_fragment);
 
         NavController navController = Navigation.findNavController(view);
 //        Property_model_class recieved_data = EditFragmentArgs.fromBundle(getArguments()).getDataForEditFragment();
-
-        propertyFragmentViewModel = new ViewModelProvider(getActivity()).get(PropertyFragmentViewModel.class);
+//        propertyFragmentViewModel = new ViewModelProvider(getActivity()).get(PropertyFragmentViewModel.class);
         propertyFragmentViewModel.getDataForPropertyFragmentToEditFragment().observe(getActivity(), new Observer<Property_model_class>() {
             @Override
             public void onChanged(Property_model_class recieved_data) {
-                edit_phoneno.setText(recieved_data.getPhone_number());
-                edit_adress.setText(recieved_data.getAdress());
-                edit_price.setText(recieved_data.getPrice());
-                edit_details.setText(recieved_data.getDetails());
+//                edit_phoneno.setText(recieved_data.getPhone_number());
+//                edit_adress.setText(recieved_data.getAdress());
+//                edit_price.setText(recieved_data.getPrice());
+//                edit_details.setText(recieved_data.getDetails());
+                fragmentEditBinding.setInflateEditFragmentData(recieved_data);
                 offered_by = recieved_data.getOfferedby();
                 edit_image_uri = null;
                 image_url = recieved_data.getProperty_image();
@@ -89,9 +96,7 @@ public class EditFragment extends Fragment {
                 property_ID_particular = recieved_data.getProperty_ID_particular();
             }
         });
-
-
-        edit_Image.setOnClickListener(new View.OnClickListener() {
+        fragmentEditBinding.editimageFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent image_picker_for_edit = new Intent();
@@ -101,7 +106,7 @@ public class EditFragment extends Fragment {
             }
         });
 
-        update_button.setOnClickListener(new View.OnClickListener() {
+        fragmentEditBinding.updatebuttonFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String image_name = System.currentTimeMillis()+"."+getExtension(edit_image_uri);
@@ -118,10 +123,10 @@ public class EditFragment extends Fragment {
                                         image_url = String.valueOf(uri);
                                         Log.d("naya url",image_url+" "+offered_by+" "+property_ID+" "+property_ID_particular);
                                         Map<String,Object> map = new HashMap<>();
-                                        map.put("phone_number",edit_phoneno.getText().toString());
-                                        map.put("adress",edit_adress.getText().toString());
-                                        map.put("price",edit_price.getText().toString());
-                                        map.put("details",edit_details.getText().toString());
+                                        map.put("phone_number",fragmentEditBinding.editphonenoFragment.getText().toString());
+                                        map.put("adress",fragmentEditBinding.editadressFragment.getText().toString());
+                                        map.put("price",fragmentEditBinding.editpriceFragment.getText().toString());
+                                        map.put("details",fragmentEditBinding.editdetailsFragment.getText().toString());
                                         map.put("offeredby",offered_by);
                                         map.put("property_image",image_url);
                                         map.put("property_ID",property_ID);
@@ -167,7 +172,7 @@ public class EditFragment extends Fragment {
             }
         });
 
-        delete_button.setOnClickListener(new View.OnClickListener() {
+        fragmentEditBinding.deletebuttonFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseStorage.getInstance().getReferenceFromUrl(image_url).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -215,7 +220,7 @@ public class EditFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==1&&data!=null){
             edit_image_uri = data.getData();
-            edit_Image.setImageURI(edit_image_uri);
+            fragmentEditBinding.editimageFragment.setImageURI(edit_image_uri);
         }
     }
 
@@ -228,5 +233,10 @@ public class EditFragment extends Fragment {
         else{
             return null;
         }
+    }
+
+    @BindingAdapter("android:loadImage")
+    public static void loadImage(ImageView property_image, String URL){
+        Picasso.get().load(URL).into(property_image);
     }
 }
